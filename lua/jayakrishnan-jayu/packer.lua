de9b22080d76f9252002690085156bd9f8978249
jayakrishnan-jayu <jayakrishnan-jayu@protonmail.com>
2023-12-05 00:32:10 +0530

    update harpoon

--- lua/jayakrishnan-jayu/packer.lua
+++ lua/jayakrishnan-jayu/packer.lua
@@ -18,8 +18,14 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  use('thePrimeagen/harpoon')
+  use "nvim-lua/plenary.nvim" 
 
+  use {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      requires = { {"nvim-lua/plenary.nvim"} }
+  }
+  
   use("mbbill/undotree")
 
   use("tpope/vim-fugitive")
