340996c862f6f274da168443cb4e677976a8fd4a
jayakrishnan-jayu <jayakrishnan-jayu@protonmail.com>
2024-07-12 20:50:00 +0530

    update harpoon

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -63,5 +63,5 @@ require('rose-pine').setup({
 
 -- Set colorscheme after options
 vim.cmd('colorscheme rose-pine')
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
